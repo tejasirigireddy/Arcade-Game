8583a3ecbaac6e04157628097740273c1f01c7a3
tejasirigireddy <48981173+tejasirigireddy@users.noreply.github.com>
2019-06-20 14:09:16 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -141,6 +141,7 @@ Player.prototype.handleInput = function(key) {
   }
 
 }
+
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
